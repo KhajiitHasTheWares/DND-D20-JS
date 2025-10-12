96f9bb208701fb040dc8cff4218d47689efde1d4
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-12 11:32:37 -0400

    Update .js
    Signed-off-by: Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>

--- .js
+++ .js
@@ -7,3 +7,9 @@ function throwDice (numRolls) {
         rollDie();
     }
  } //This is a D20 random roller program like a DND D20 die
+
+if(numRolls > 15) {
+    console.log("You rolled awesome!");
+}else {
+    console.log("Aww, better luck next time!");
+}
