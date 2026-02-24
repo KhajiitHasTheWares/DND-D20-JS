dee9d92877c7c5a18d26e8bd89882c80846ecdc1
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2026-02-24 09:17:50 -0500

    Refactor dice roll feedback using switch statement
    Replaced if-else structure with a switch statement to evaluate dice rolls and provide feedback based on the result.
    
    Signed-off-by: Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>

--- .js
+++ .js
@@ -8,8 +8,24 @@ function throwDice (numRolls) {
     }
  } //This is a D20 random roller program like a DND D20 die
 
-if(numRolls > 15) {
-    console.log("You rolled awesome!");
-}else {
-    console.log("Aww, better luck next time!");
-}
+const expr = "diceRoll";
+switch (expr) {
+    case < 5:
+        console.log("Aww, better luck next time!");
+        break;
+    case > 5 && < 10:
+        console.log("Not too bad, but you need to have more luck!");
+        break;
+    case > 10 && < 15:
+        console.log("You rolled a bit better there!");
+        break;
+    case < 15 && > 20:
+        console.log("Nice lucky roll!");
+        break;
+    case 20:
+        console.log("Incredible!!!!");
+        break;
+    default:
+        console.log('Try the roll again!");
+                    }
+//The switch statement runs the text that shows up when the virtual die is rolled
